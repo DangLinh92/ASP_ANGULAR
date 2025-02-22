9fee13b2bbb7a0bf3b1103182c562faebfec44dd
danglv <danglevan.9919@gmail.com>
2025-02-22 10:13:36 +0700

    init Blog source code

---
+++ Tedu/tedu-blog-lessions-4.1/tedu-blog-lessions-4.1/src/TeduBlog.Core/SeedWorks/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using TeduBlog.Core.Repositories;
+
+namespace TeduBlog.Core.SeedWorks
+{
+    public interface IUnitOfWork
+    {
+        IPostRepository Posts { get; }
+        IPostCategoryRepository PostCategories { get; }
+        ISeriesRepository Series { get; }
+        ITransactionRepository Transactions { get; }
+        IUserRepository Users { get; }
+
+        Task<int> CompleteAsync();
+    }
+}
